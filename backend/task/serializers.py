9501df352cf0699067951a6396f93ae864ae3144
Kyle Wisecarver <wiseworks.2022@gmail.com>
2023-02-15 12:06:18 -0600

    chore: Viewset, urls.py updated. Working admin, api, api/task screens. Creating new branch for front end.

---
+++ backend/task/serializers.py
@@ -0,0 +1,11 @@
+# import serializers from the REST framework
+from rest_framework import serializers
+
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'description', 'completed', 'task_image')
+        
+        
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# import Views sets from REST framework
+from rest_framework import viewsets
 
+# import the TaskSerializer
+from .serializers import TaskSerializer
+
+# import the Task model
+from .models import Task
+
+# create a class for the Task model viewsets
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 # Create your views here.
